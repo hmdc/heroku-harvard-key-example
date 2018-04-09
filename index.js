1b104f24bf330fe5813e25050dee7de0ca8ee7da
Evan Sarmiento <esarmien@g.harvard.edu>
2018-04-09 10:42:06 -0400

    Add else

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('You got through!'))
+
+app.listen(12000, () => console.log('Example app listening on port 12000!'))
